6abdd37b0404abdb367fbe9ecf8f3acceae58942
clayton <marquesclayton1@gmail.com>
2026-02-11 21:33:15 -0300

    auala 3

---
+++ src/main/java/com/br/senai/ads3/agenda_fatesg/controllers/ContatoController.java
@@ -0,0 +1,43 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.br.senai.ads3.agenda_fatesg.controllers;
+
+import com.br.senai.ads3.agenda_fatesg.domains.Contato;
+import java.util.List;
+
+/**
+ *
+ * @author Clayton
+ */
+public class ContatoController implements IContatoCrudController{
+
+    @Override
+    public boolean insert(Contato contato) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public boolean update(Contato contato) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public boolean delete(Contato contato) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public List<Contato> searchAll() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public boolean exists(Contato contato) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+    
+    
+}
